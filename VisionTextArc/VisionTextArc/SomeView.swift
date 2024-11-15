bb44ef0b89157574ab704ee83f7de30beda01e13
Dave-Ed-Cast <148765897+Dave-Ed-Cast@users.noreply.github.com>
2024-11-15 15:27:26 +0100

    Commit stu maronn

---
+++ VisionTextArc/VisionTextArc/SomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SomeView.swift
+//  VisionTextArc
+//
+//  Created by Davide Castaldi on 15/11/24.
+//
+
+import SwiftUI
+
+struct SomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SomeView()
+}
