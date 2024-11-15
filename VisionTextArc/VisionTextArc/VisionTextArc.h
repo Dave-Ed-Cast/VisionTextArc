895c4d53a35e474c4fa5cc388a000992866929e8
Dave-Ed-Cast <148765897+Dave-Ed-Cast@users.noreply.github.com>
2024-11-15 15:12:02 +0100

    First commit

---
+++ VisionTextArc/VisionTextArc/VisionTextArc.h
@@ -0,0 +1,18 @@
+//
+//  VisionTextArc.h
+//  VisionTextArc
+//
+//  Created by Davide Castaldi on 15/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VisionTextArc.
+FOUNDATION_EXPORT double VisionTextArcVersionNumber;
+
+//! Project version string for VisionTextArc.
+FOUNDATION_EXPORT const unsigned char VisionTextArcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisionTextArc/PublicHeader.h>
+
+
